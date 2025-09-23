24d61b0fa8dc22275cb04d3eccd1d56fd450a0e9
leemonyung-code <leemonyung@gmail.com>
2025-09-23 00:10:55 -0700

    Add files via upload

---
+++ lab1/converter.js
@@ -0,0 +1,14 @@
+// Temperature Converter
+
+// Set fahrenheit variable
+let fahrenheit = 57 // Average temperature in Dali, Yunnan Province, China in November is about 14°C
+
+// Convert to celsius
+let celsius = (fahrenheit - 32) * 5 / 9
+
+// Convert to kelvin
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+// Output results with labels
+console.log(celsius)
+console.log(kelvin)
